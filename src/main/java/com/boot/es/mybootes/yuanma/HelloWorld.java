2184a0fa2c78f3c5d55dac7e69b693a8dd0f108d
sunwenlong <644013561@qq.com>
2018-05-07 15:17:12 +0800

    取出map里面的值

--- src/main/java/com/boot/es/mybootes/yuanma/HelloWorld.java
+++ src/main/java/com/boot/es/mybootes/yuanma/HelloWorld.java
@@ -1,11 +1,26 @@
 package com.boot.es.mybootes.yuanma;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public class HelloWorld {
+
+    private int age;
+
+    private String name;
+
+    public HelloWorld(int age,String name){
+        this.age=age;
+        this.name=name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public static void main(String[] args) {
         Collection con=new Vector();
         //((Vector) con).add(0,"23");
@@ -14,6 +29,36 @@ public class HelloWorld {
         for(Object obj:list){
             System.out.println(obj);
         }
+
+
+
+        //map 取值的方式
+        Map<String,String> map = new HashMap<>();
+        map.put("123","321");
+        map.put("aaa","vbbb");
+        //---------第一种方式-------
+        //        Iterator iterator = map.entrySet().iterator();
+//        while(iterator.hasNext()){
+//            Map.Entry entry= (Map.Entry)iterator.next();
+//            System.out.println(entry.getKey());
+//            System.out.println( entry.getValue());
+//
+//        }
+        for (Object o : map.entrySet()) {
+            Map.Entry entry = (Map.Entry) o;
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+
+        }
+
+        //---------第二种方式-------
+        for(Object o: map.keySet()){
+            System.out.println(o);
+            System.out.println(map.get(o));
+        }
+
+
+
 
     }
 }
